0fd55f3490bcf359a6ca85bed745a15ef7ba240a
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-03-01 23:41:50 -0500

    Fetch data from Json

--- app/src/command_line_interface/index.js
+++ app/src/command_line_interface/index.js
@@ -1,35 +1,81 @@
 import inquirer from 'inquirer';
+import fs from 'fs'
+import data from '../../data.json' assert { type: "json" };
 
-let API_KEY = ""; //For dev only, it should be returned from the api module
+const ACTIONS = {
+    EXPORT_EXCEL: "exportExcel",
+    SET_API_KEY: "setApiKey",
+    SET_WROKSPACE_ID: "setWorkplaceId",
+    EXIT: "exit"
+};
+
+const APIKeyPrompt = async function () {
+  const APIKeyPrompt = inquirer.createPromptModule();
+  await APIKeyPrompt([
+    {
+      type: "input",
+      name: "key",
+      message: "Please provide your api key",
+    },
+  ])
+    .then((answers) => {
+      //Update API_KEY in JSON file
+      data.API_KEY = answers.key;
+      fs.writeFile("data.json", JSON.stringify(data), function () {});
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export async function startCli(){
 
-    //Ask the user to setup API key if it is not setup yet
-    if (!API_KEY || API_KEY === "") {
-        const APIKeyPrompt = inquirer.createPromptModule();
-        await APIKeyPrompt([{
-            type: "input",
-            name: "key",
-            message: "Please provide your api key"
-        }]).then((answers) => {
-            API_KEY = answers.key;
-        }).catch((error) => {
-            console.log(error)
-        });
-    };
+    let action;
 
-    console.log("API_KEY:", API_KEY)
+    //Ask the user to setup API key if it is not setup yet
+    if (!data.API_KEY || data.API_KEY === "") await APIKeyPrompt();
 
     //Ask the user for the next actions
     const actionPrompt = inquirer.createPromptModule();
     await actionPrompt([{
-        type: "",
-        name: "",
-        message: ""
-    }]).then((answers) => {
-
+        type: "rawlist",
+        name: "action",
+        message: "Please select an option below",
+        choices: [
+            {
+                name: "Export Timesheet to Excel File", 
+                value: ACTIONS.EXPORT_EXCEL
+            },{
+                name: "Update API Key", 
+                value: ACTIONS.SET_API_KEY
+            }, {
+                name: "Update Workspace ID", 
+                value: ACTIONS.SET_WROKSPACE_ID
+            }, {
+                name: "Exit", 
+                value: ACTIONS.EXIT
+            }
+    ]}]).then((answers) => {
+        action = answers.action;
     }).catch((error) => {
         // console.log(error)
     });
 
+    //Switch according to the action
+    switch(action){
+        case ACTIONS.EXPORT_EXCEL:
+            break;
+        case ACTIONS.SET_API_KEY:
+            await APIKeyPrompt();
+            console.log("API Key is updated to", data.API_KEY);
+            break;
+        case ACTIONS.SET_WROKSPACE_ID:
+            break;
+        case ACTIONS.EXIT:
+            console.log("Terminating Application");
+            process.exit();
+    };
+
+    await startCli()
 }
+
